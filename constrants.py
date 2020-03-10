4b84ec01c87d4f6e4c8b48524ae50df6ded6758f
LinXunFeng <xunfenghellolo@gmail.com>
2020-03-10 18:21:58 +0800

    定义常量

---
+++ constrants.py
@@ -0,0 +1,4 @@
+ITMSTRANSPORTER_ID = "com.apple.amp.itmstransporter"
+ITMSTRANSPORTER_DOWNLOAD_URL = "https://github.com/LinXunFeng/com.apple.amp.itmstransporter/archive/master.zip"
+FILES_DIR_NAME = "files"
+REPOSITORY_XML_RELEATIVE_PATH = 'obr/2.0.0/repository.xml'
