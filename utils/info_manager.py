e86a8b23c2a5110a327450fa9acd57613ccd1af8
LinXunFeng <xunfenghellolo@gmail.com>
2020-03-11 12:14:06 +0800

    添加获取当前同级目录路径的方法

--- utils/info_manager.py
+++ utils/info_manager.py
@@ -1,5 +1,5 @@
-import os
-
+import os, sys
+from pathlib import Path
 
 def get_user():
     """获取当前系统用户名"""
@@ -10,6 +10,16 @@ def get_user():
 def get_cache_path():
     """获取系统cache路径"""
     return "/Users/{}/Library/Caches".format(get_user())
+
+
+def is_exe():
+    """是否是可执行文件"""
+    return sys.argv[0].split('/')[-1].find('.py') == -1
+
+
+def get_current_dir_path():
+    """获取当前执行文件的目录路径"""
+    return Path(sys.argv[0]).parent if is_exe() else "{}".format(os.getcwd())
 
 
 if __name__ == '__main__':
