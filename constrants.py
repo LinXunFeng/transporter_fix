aeae6dbec289c05dfc6018752d74a3434dc2e2b1
LinXunFeng <598600855@qq.com>
2020-12-30 18:34:01 +0800

    更新itmstransporter上传工具的版本号

--- constrants.py
+++ constrants.py
@@ -1,4 +1,4 @@
 ITMSTRANSPORTER_ID = "com.apple.amp.itmstransporter"
 ITMSTRANSPORTER_DOWNLOAD_URL = "https://github.com/LinXunFeng/com.apple.amp.itmstransporter/archive/master.zip"
 FILES_DIR_NAME = "files"
-REPOSITORY_XML_RELEATIVE_PATH = 'obr/2.0.0/repository.xml'
+REPOSITORY_XML_RELEATIVE_PATH = 'obr/2.1.0/repository.xml'
